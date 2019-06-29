f97c70cdf55ec9213f99fad1590376310496adc8
Alysson Brito <alysson.brito@accedo.tv>
2019-06-29 13:16:14 -0300

    Exercise 3.1 part 2

--- chapter03/evaluating_31.erl
+++ chapter03/evaluating_31.erl
@@ -4,8 +4,11 @@
 % Exercise 3.1
 
 -module(evaluating_31).
--export([sum/1]).
+-export([sum/1,sum/2]).
 
 sum(0) -> 0;
 sum(X) -> X + sum(X-1).
+
+sum(N,M) when N == M -> 0;
+sum(N,M) when N =< M -> N + sum(N+1,M).
 
