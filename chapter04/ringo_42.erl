601f770249ce693bf640d406558cc8c765a6e4aa
Alysson Brito <alysson.brito@accedo.tv>
2019-07-05 00:03:32 -0300

    Code clean up

--- chapter04/ringo_42.erl
+++ chapter04/ringo_42.erl
@@ -10,26 +10,27 @@
 %% ---------------- interface
 start(M, N, Message) ->
     Process_List = create_N_process(N),
-    %io:format("Process List:~p~n",[ Process_List]),
     connect_in_ring(Process_List),
-    %io:format("Sending messages...~n"),
     send_message_to_ring(Process_List,Message,M*N),
     send_quit(Process_List).
 
+% creates N process and return Pids as a list
 create_N_process(0) -> [];
 create_N_process(N) ->
     Pid = spawn(ringo_42,init,[]),
-    %io:format("Creating process Pid:[~p]~n",[ Pid ]),
     [Pid | create_N_process(N-1)].
 
-
+%% starts off the process
 init() ->
     loop(null).
 
+%% connects process as a circular list, link all to the next
+%% and the latest to the head
 connect_in_ring(ProcessList) ->
     io:format("Calling connect in ring [~p]~n",[ProcessList ++ [hd(ProcessList)]]),
     connect_in_ring_(ProcessList ++ [hd(ProcessList)]).
 
+%helper function
 connect_in_ring_( [H | [] ]) -> [H];
 connect_in_ring_([ H | T]) ->
     %io:format("Setting next:H[~p] T:[~p] TT:[~p]~n",[H,hd(T),T]),
@@ -37,18 +38,18 @@ connect_in_ring_([ H | T]) ->
     connect_in_ring_(T).
 
 
-%head, list of process
+% inform each process who is the next one
 set_next(A,B) ->
     A ! {set_next, B}.
 
-% kill them all!
+% burtn them all!
 send_quit([]) -> done;
 send_quit([H | T]) ->
     io:format("Sending QUIT to H:[~p]~n",[ H ]),
     H ! {quit},
     send_quit(T).
 
-% process A (next,)
+% main loop A
 loop(Next) ->
     receive
 	{set_next, ProcessPid} ->
@@ -57,27 +58,15 @@ loop(Next) ->
 	    Sender ! {ack, self()},
 	    loop(Next);
 	{send_message, Sender, Msg, MN } ->
-	    io:format("-------------------------------------------------------~n"),
-	    io:format("Self:[~p] Msg:[~p]~n",[ self() ,Msg ]),
-	    io:format("Self:[~p] Msg:[~p]~n",[ self() ,Msg ]),
-	    io:format("Self:[~p] Msg:[~p]~n",[ self() ,Msg ]),
-	    io:format("Self:[~p] Msg:[~p]~n",[ self() ,Msg ]),
+	    io:format("Self:[~p] Msg:[~p] MN[~p] ~n",[ self() ,Msg , MN]),
 	    Next ! {send_message, Sender, Msg, MN-1},
-	    io:format("Looping~n"),
 	    loop(Next);
-	{quit} -> done;
-	{ack, _Pid} -> done
+	{quit} -> done
     end.
 
 send_message_to_ring(ProcessList, Message, M) ->
-   % io:format("Sending M[~p] message to:[~p]~n",[M, hd(ProcessList)]),
     hd(ProcessList) ! {send_message, self(), Message, M},
-    %send_M_messages(ProcessList, Message, M-1),
     receive
 	{ack, _Pid} -> quit
     end.
-
-
-
-
 
