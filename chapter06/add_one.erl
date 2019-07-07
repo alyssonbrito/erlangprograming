0e16089e3bda10edfbf3632eb3a506087ec4d862
Alysson Brito <alysson.brito@accedo.tv>
2019-07-07 16:30:04 -0300

    add_one example

---
+++ chapter06/add_one.erl
@@ -0,0 +1,22 @@
+% Examples from Erlang Programming (2009), by Francesco Cesarini and Simon Thompson
+% Example 6.1. Process Links and exit signals
+
+-module(add_one).
+-export([start/0, request/1, loop/0]).
+
+start() ->
+    register(add_one, spawn_link(add_one, loop, [])).
+
+request(Int) ->
+    add_one ! {request, self(), Int},
+    receive
+	{result, Result} -> Result
+    after 1000 -> timeout
+    end.
+
+loop() ->
+    receive
+	{request, Pid, Msg} ->
+	    Pid ! {result, Msg + 1}
+    end,
+    loop.
