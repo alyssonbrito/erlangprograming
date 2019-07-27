3af3f15ecdb998fae3306585b155401737b4db41
Alysson Brito <alysson.brito@accedo.tv>
2019-07-26 21:53:01 -0300

    Up to event.handler. Not all exercises, examples

---
+++ learnyousomeerlang/event.handler/curling_feed.erl
@@ -0,0 +1,33 @@
+%% @reference Learn You Some Erlang for great good
+%% Event handler
+%% Curling Time !
+
+-module(curling_feed).
+-behaviour(gen_event).
+ 
+-export([init/1, handle_event/2, handle_call/2, handle_info/2, code_change/3,
+        terminate/2]).
+ 
+init([Pid]) ->
+    {ok, Pid}.
+ 
+handle_event(Event, Pid) ->
+    Pid ! {curling_feed, Event},
+    {ok, Pid}.
+ 
+handle_call(_, State) ->
+    {ok, ok, State}.
+ 
+handle_info(_, State) ->
+    {ok, State}.
+ 
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+ 
+terminate(_Reason, _State) ->
+    ok.
+
+% c(curling), c(curling_feed).
+% {ok, Pid} = curling:start_link("Saskatchewan Roughriders", "Ottawa Roughriders").
+
+
